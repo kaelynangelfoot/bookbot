3e8d801726448d84445b2e049c5075400d70ccc6
kaelynangelfoot <renee@angelfootenterprises.com>
2025-03-17 19:15:42 -0400

    boot.dev ch2 lesson 3 bookbot

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+# the follwing function will pull the text from a file and convert it to a string
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+def main():
+    # Call get_book_text with the path to your file
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+#main
+main()

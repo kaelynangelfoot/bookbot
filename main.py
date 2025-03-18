fd7691b1ccaeb1baa15cffa110690055d94efaff
kaelynangelfoot <renee@angelfootenterprises.com>
2025-03-18 15:47:39 -0400

    FINALLY I HATE THIS PROJECT

--- main.py
+++ main.py
@@ -3,25 +3,44 @@ num_words = 0
 book_text = ()
 words = []
 
-# the follwing function will pull the text from a file and convert it to a string
-def get_book_text(filepath):
-    with open(filepath) as f:
-        return f.read()
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
 
 def main():
-    # Call get_book_text with the path to your file
-    book_text = get_book_text("books/frankenstein.txt")
-    num_words = word_count(book_text)
-    print(num_words, "words found in the document")
-    return word_count
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
-from stats import word_count
-from stats import character_count
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-print (character_count)
 
-#main
-if __name__ == "__main__":
-    main()
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
 
+    print("============= END ===============")
+
+
+main()
--- stats.py
+++ stats.py
@@ -1,12 +1,26 @@
-# These are the functions we will use to assess book documents
-
-def word_count(book_text):
-    # This function returns word count for the text in question
-    words = book_text.split()
+def get_num_words(text):
+    words = text.split()
     return len(words)
 
-def character_count(words):
-    # This functions will reduce all characters to lowercase and return a dictionary with the character and the number of times it occurs
-    lowercase = words.lower()
-    return lowercase
 
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
